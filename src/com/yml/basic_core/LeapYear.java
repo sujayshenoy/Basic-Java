0176cbe3c6172319f4fea7048c635c543c16c882
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-08-24 14:26:51 +0530

    added class to check if a year is leap or not

---
+++ src/com/yml/basic_core/LeapYear.java
@@ -0,0 +1,35 @@
+package com.yml.basic_core;
+
+import java.util.*;
+import java.io.*;
+
+public class LeapYear {
+	
+	public static void run() {
+		
+		Scanner in = new Scanner(System.in);
+		
+		print("Enter the year");
+		int year = in.nextInt();
+		
+		while( String.valueOf(year).length() != 4 || year < 0) {
+			print("Year should be a 4 digit positive number");
+			year = in.nextInt();
+		}
+		
+		if(((year%4 == 0) && (year%100 != 0)) || (year%400 == 0)) {
+			print(""+year+" is a leap year");
+		}
+		else {
+			print(""+year+" is not a leap year");
+		}
+		
+	}
+	
+	private static void print(String msg) {
+		PrintWriter out = new PrintWriter(System.out);
+		out.println(msg);
+		out.flush();
+	}
+	
+}
